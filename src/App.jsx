c7bf90dae597a97cb6b13ffd1c11989697435666
AndyHou <andyhou965@gmail.com>
2024-08-07 23:06:09 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <Button className="m-2">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
